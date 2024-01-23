39195e1b508f81f6f8a476fa15188c8b3a85105b
KamisatoKurumi <1836312914@qq.com>
2024-01-24 02:41:48 +0800

    做完了钥匙和门的机关

---
+++ Assets/Scripts/Player/PlayerTag.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTag : MonoBehaviour
+{
+    public PlayerType _tag;
+}
+
+public enum PlayerType
+{
+    A,B
+}
